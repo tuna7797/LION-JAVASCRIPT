7e1cb05027baa59bc8a563cc9e0647f73bc9c8d2
tuna7797 <gmlwjd7731@gmail.com>
2023-07-07 12:03:46 +0900

    add. 5일차 수업 - for 반복문 - for..in (hasOwnProperty)

--- client/chapter/core/09-2.loop.js
+++ client/chapter/core/09-2.loop.js
@@ -81,3 +81,19 @@ console.log(prev(second));
 // console.log(div);
 
 // 로직 실행
+
+// let loginCount = 0;
+
+// if(로그인){
+
+//   if(본인인증){
+//     if(비밀번호찾기){
+//       if(본인인증){
+//         if(비밀번호바꾸기){
+
+//         }
+//       }
+//     }
+//   }
+//   loginCount += 2;
+// }
